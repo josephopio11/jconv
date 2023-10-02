4a89d94ce41c2b0c027e926cd7b0d3a133348624
Joseph Opio <josephopio11@gmail.com>
2023-10-02 20:34:36 +0800

    Initial Commit

---
+++ components/navbar.tsx
@@ -0,0 +1,98 @@
+// imports
+import Image from 'next/image';
+import { Button } from './ui/button';
+import Link from 'next/link';
+import { BsGithub } from 'react-icons/bs';
+import { GrMenu } from 'react-icons/gr';
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet';
+
+export default function Navbar({}): any {
+  return (
+    <nav className="fixed z-50 flex items-center justify-between w-full h-24 px-4 py-10 bg-white backdrop-blur-md bg-opacity-30 md:px-8 lg:px-12 xl:px-16 2xl:px-24">
+      <Link href="/">
+        <Image
+          alt="logo"
+          className="w-40 cursor-pointer"
+          src="/images/logo.svg"
+          height={100}
+          width={170}
+        />
+      </Link>
+      <div className="hidden gap-1 md:gap-2 lg:gap-4 md:flex">
+        <Button variant="ghost" className="font-semibold text-md">
+          <Link href="/">Home</Link>
+        </Button>
+        <Link href="/about">
+          <Button variant="ghost" className="font-semibold text-md">
+            About
+          </Button>
+        </Link>
+        <Link href="/privacy-policy">
+          <Button variant="ghost" className="font-semibold text-md">
+            Privacy Policy
+          </Button>
+        </Link>
+      </div>
+      <Link href="https://github.com/josephopio11/">
+        <Button
+          variant="default"
+          className="items-center hidden gap-2 bg-orange-600 rounded-full w-fit md:flex"
+          size="lg"
+        >
+          <span>Github Repo</span>
+          <span className="text-xl">
+            <BsGithub />
+          </span>
+        </Button>
+      </Link>
+
+      {/* MOBILE NAV */}
+      <Sheet>
+        <SheetTrigger className="block p-3 md:hidden">
+          <span className="text-2xl">
+            <GrMenu />
+          </span>
+        </SheetTrigger>
+        <SheetContent>
+          <SheetHeader>
+            <SheetDescription>
+              <div className="w-full space-y-3">
+                <Link href="/">
+                  <Button
+                    variant="link"
+                    className="w-full font-semibold text-md"
+                  >
+                    Home
+                  </Button>
+                </Link>
+                <Link href="/about">
+                  <Button
+                    variant="link"
+                    className="w-full font-semibold text-md"
+                  >
+                    About
+                  </Button>
+                </Link>
+                <Link href="/privacy-policy">
+                  <Button
+                    variant="link"
+                    className="w-full font-semibold text-md"
+                  >
+                    Privacy Policy
+                  </Button>
+                </Link>
+              </div>
+            </SheetDescription>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
+    </nav>
+  );
+}
