44b93ae24b383eeeae848c9b1907f88942dfdd6a
Joseph Opio <josephopio11@gmail.com>
2023-10-05 22:25:56 +0800

    revert

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -27,7 +27,7 @@ export default function Navbar({ }): any {
       </Link>
       <div className="hidden gap-1 md:gap-2 lg:gap-4 md:flex">
         <Button variant="ghost" className="font-semibold text-md">
-          <Link href="https://www.josephopio.com">Home Page</Link>
+          <Link href="https://www.josephopio.com">Home</Link>
         </Button>
         <Link href="/about">
           <Button variant="ghost" className="font-semibold text-md">
@@ -69,7 +69,7 @@ export default function Navbar({ }): any {
                     variant="link"
                     className="w-full font-semibold text-md"
                   >
-                    Home Page
+                    Home
                   </Button>
                 </Link>
                 <Link href="/about">
