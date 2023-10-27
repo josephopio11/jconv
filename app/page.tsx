6c011156ecd03662e36af1938511efc60abe4db7
Joseph <josephopio11@gmail.com>
2023-10-27 18:38:26 +0800

    messed up

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,16 @@
 // imports
 import Dropzone from '@/components/dropzone';
+import React from 'react';
 
 export default function Home() {
   return (
-    <div className="space-y-16 pb-8">
+    <div className="pb-8 space-y-16">
       {/* Title + Desc */}
       <div className="space-y-6">
-        <h1 className="text-3xl md:text-5xl font-medium text-center">
+        <h1 className="text-3xl font-medium text-center md:text-5xl">
           Free Unlimited Media Files Converter
         </h1>
-        <p className="text-gray-400 text-md md:text-lg text-center md:px-24 xl:px-44 2xl:px-52">
+        <p className="text-center text-gray-400 text-md md:text-lg md:px-24 xl:px-44 2xl:px-52">
           Unleash your creativity with FreeConverter – the ultimate online tool for
           unlimited and free multimedia conversion. Transform images, audio, and
           videos effortlessly, without restrictions. Start converting now and
